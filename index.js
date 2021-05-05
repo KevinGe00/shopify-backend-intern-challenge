07929c2f0b167a3f2e7a426b0173b9267eac673b
Kevin Ge <kevinge@Garys-MacBook-Air.local>
2021-05-05 17:07:20 -0400

    moving routes, using index.js instead, added .env file

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose')
 require('dotenv').config();
 
 const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-app.use((req, res, next) => {
-  res.send('Welcome to Express');
-});
-
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
+  console.log(`Listening on port ${port}`)
 });
