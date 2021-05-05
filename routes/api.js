e92312530bc7932aa92f2b085888c6422703788a
Kevin Ge <kevinge@Garys-MacBook-Air.local>
2021-05-05 16:29:50 -0400

    set up nodejs server and routes using express router

---
+++ routes/api.js
@@ -0,0 +1,13 @@
+const express = require ('express');
+const router = express.Router();
+
+
+router.post('/images', (req, res, next) => {
+
+});
+
+router.delete('/images/:id', (req, res, next) => {
+
+})
+
+module.exports = router;
