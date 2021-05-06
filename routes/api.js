f151fcc2edcee411c4c345245dc083a949624977
Kevin Ge <kevinge@Garys-MacBook-Air.local>
2021-05-06 17:12:25 -0400

    set up multer in api.js

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,19 @@
 const express = require ('express');
+const multer = require('multer');
 const router = express.Router();
 
+
+// Set up multer to store uploaded files
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '-' + Date.now())
+    }
+});
+ 
+const upload = multer({ storage: storage });
 
 router.get('/images', (req, res) => {
 
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose')
-const routes = require('./routes/api');
 const fs = require('fs');
 const path = require('path');
 require('dotenv').config();
 
 const app = express();
+
 const port = process.env.PORT || 5000;
 
 // Connecting to database
@@ -16,6 +16,19 @@ mongoose.connect(
       console.log('Successfully connected to database')
   }
 );
+
+//
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+      cb(null, 'uploads')
+  },
+  filename: (req, file, cb) => {
+      cb(null, file.fieldname + '-' + Date.now())
+  }
+});
+
+const upload = multer({ storage: storage });
 
 app.use('/api', routes);
 
