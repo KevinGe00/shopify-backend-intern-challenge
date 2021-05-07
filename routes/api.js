a4a7ce585f130b68d4f94c5fc4c774ef1b41dcbc
Kevin Ge <kevinge@Garys-MacBook-Air.local>
2021-05-06 21:08:12 -0400

    created post request to upload images to database

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,11 @@
 const express = require ('express');
 const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
 const router = express.Router();
+
+// Load mongoose models
+const imageModel = require('../models/image');
 
 
 // Set up multer to store uploaded files
@@ -12,11 +17,30 @@ const storage = multer.diskStorage({
         cb(null, file.fieldname + '-' + Date.now())
     }
 });
- 
 const upload = multer({ storage: storage });
 
-router.get('/images', (req, res) => {
 
+// Upload photos sent by users to mongodb database
+router.post('/upload', upload.single('image'), (req, res) => {
+    const image = {
+        name: req.body.name,
+        desc: req.body.desc,
+        img: {
+            data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),
+            contentType: 'image/png'
+        }
+    }
+
+    // Save to DB using schema for images
+    imageModel.create(image, (err, item) => {
+        if (err) {
+            console.log(err)
+            res.status(500).send("Internal server error");
+        } else {
+            // Created model success and saved to db
+            res.status(201).send()
+        }
+    })
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose')
+const routes = require('./routes/api');
 const fs = require('fs');
 const path = require('path');
 require('dotenv').config();
@@ -11,24 +12,11 @@ const port = process.env.PORT || 5000;
 
 // Connecting to database
 mongoose.connect(
-  process.env.MONGODB_URL,
+  process.env.MONGODB_URL || "mongodb://localhost:27017/images",
   { useNewUrlParser: true, useUnifiedTopology: true }, err => {
       console.log('Successfully connected to database')
   }
 );
-
-//
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-      cb(null, 'uploads')
-  },
-  filename: (req, file, cb) => {
-      cb(null, file.fieldname + '-' + Date.now())
-  }
-});
-
-const upload = multer({ storage: storage });
 
 app.use('/api', routes);
 
