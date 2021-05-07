0090baac896fccbc9ac8747941e7f44c17e321e7
Kevin Ge <kevinge@Garys-MacBook-Air.local>
2021-05-07 19:42:57 -0400

    fixed CORS error

--- server.js
+++ server.js
@@ -8,13 +8,22 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // Connecting to database
 mongoose.connect(
   process.env.MONGODB_URL || "mongodb://localhost:27017/images",
   { useNewUrlParser: true, useUnifiedTopology: true }, err => {
-      console.log('Successfully connected to database')
+    console.log('Successfully connected to database')
   }
 );
+
+// Support parsing of application/json type post data
+app.use(bodyParser.json());
 
 app.use('/api', routes);
 
