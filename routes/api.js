f7045d7a79df9c636316c2466fe9bc4aae993e66
Kevin Ge <kevinge@Garys-MacBook-Air.local>
2021-05-07 22:25:01 -0400

    fix server bugs when trying to upload photo from frontend to mongodb

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,4 @@
-const express = require ('express');
+const express = require('express');
 const multer = require('multer');
 const fs = require('fs');
 const path = require('path');
@@ -11,22 +11,23 @@ const imageModel = require('../models/image');
 // Set up multer to store uploaded files
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'uploads')
+        cb(null, path.join(__dirname, '/images/'));
     },
     filename: (req, file, cb) => {
-        cb(null, file.fieldname + '-' + Date.now())
+        cb(null, file.originalname + '-' + Date.now())
     }
 });
 const upload = multer({ storage: storage });
 
 
 // Upload photos sent by users to mongodb database
-router.post('/upload', upload.single('image'), (req, res) => {
+router.post('/upload', upload.single('img'), (req, res) => {
+
     const image = {
         name: req.body.name,
         desc: req.body.desc,
         img: {
-            data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),
+            data: fs.readFileSync(path.join(__dirname + '/images/' + req.file.filename)),
             contentType: 'image/png'
         }
     }
